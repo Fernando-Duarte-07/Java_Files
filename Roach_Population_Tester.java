d60cba6963e47da68108018d5cf571036663f2c7
SenBellum <48704284+SenBellum@users.noreply.github.com>
2020-02-17 11:29:56 -0800

    Added scenario 1

--- Roach_Population_Tester.java
+++ Roach_Population_Tester.java
@@ -1,14 +1,16 @@
 package cecs274;
 
-public class Roach_Population_Tester {
+public class speciestest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Roach_Population rs = new Roach_Population();
 		System.out.println(rs);
 		rs.setPop(1000);
-		rs.getPop();
-		rs.setGang();
+		rs.getpop();
+		rs.SetGang("qwerty");
+		rs.getgang();
+		System.out.println(rs);
 	}
 
 }
