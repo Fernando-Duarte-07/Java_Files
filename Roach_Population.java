53deb86644ebf987648e3f67b0da44421ac56221
Perrari <48600510+Perrari@users.noreply.github.com>
2020-02-18 22:03:32 -0800

    fixes around the board
    fixed mathematical issues with String toString. fixed breed and spray methods. rephrased the sentence for the String toString return type

--- Roach_Population.java
+++ Roach_Population.java
@@ -1,49 +1,68 @@
+/*Programmers: Fernando Duarte and James Pham
+  Date: 18 February 2020
+  Purpose: The program is used to calculate the existing population of an endangered species
+  Inputs: The name, population, breed method, spray
+  Output: the initial population, the amount the population has grown and total population after the breed and spray method
+ */
 package cecs274;
 
 public class Roach_Population {
-	private int pop;
+	public int pop;
 	private String gang;
 	private int breed;
-	double spray;
+	private double spray;
+
+	//default constructor for Roach_population
 	public Roach_Population() {
 		pop = 10;
 		gang = "Ewwww";
-		breed = pop * pop;
-		spray = breed - (breed * 0.20);
+		breed = 2;
+		spray = 0.10;
+		
 	}
+	//second constructor for altering the parameters in the tester files
 	public Roach_Population (int population, String gangs, int breeds, double sprays) {
 		population = pop;
 		gangs = gang;
 		breeds = breed;
 		sprays = spray;
 	}
+	//returns the population variable
 	public int getpop() {
 		return pop;
 	}
+	//returns the gang name variable
 	public String getgang() {
 		return gang;
 	}
-	public int getbreed() {
+	//returns the integer for the breed method
+	public int getBreed() {
 		return breed;
 	}
-	public double getspray() {
+	//returns the spray variable set earlier
+	public double getSpray() {
 		return spray;
 	}
+	//sets the variable to public for changing in the tester file
 	public void setPop(int population) {
-		population = pop;
+		pop = population;
 	}
+	//sets the variable to public for changing in the tester file
 	public void SetGang(String gangs) {
-		gangs = gang;
+		gang = gangs;
 	}
+	//sets the variable to public for changing in the tester file
 	public void setBreed(int breeds) {
-		breeds = breed;
+		breed = breeds;
 	}
+	//sets the variable to public for changing in the tester file
 	public void setSpray(double sprays) {
-		sprays = spray;
+		spray = sprays;
 	}
+	//prints out the first constructor and is able to be changed in the tester file
 	public String toString() {
 		return ("The name of the roach population is: " + gang + ". The initial population is: " + pop + ". \n" + 
-	"The population squared is:" + breed + "." + " The total population of roaches after being sprayed is: " + spray);
+	"The population grew by: " + breed + " time(s). The new population is: " + breed * pop + "." + " The percentage of roaches sprayed is: " + spray *100 + "% The total population of roaches after being sprayed is: " + ((pop * breed) - ((pop * breed) * spray)));
 	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
